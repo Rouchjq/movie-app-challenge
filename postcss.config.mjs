84e32b3ff69f6529b462c416ca2124dd9d3dd6b1
rouchjq <rouchjq8@gmail.com>
2024-12-08 21:24:40 -0400

    init repo

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+	plugins: {
+		tailwindcss: {},
+		autoprefixer: {},
+		'postcss-import': {},
+		'postcss-nested': {},
+		'postcss-extend-rule': {},
+	},
 };
 
 export default config;
